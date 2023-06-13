db2ec9ba296880cc7761a1f38eaa088c6ec36781
Julieta Negrete <juliinegrete@gmail.com>
2023-06-13 00:35:30 -0300

    Se diseño la pantalla home

--- app/src/main/java/com/ispc/gestorstock/activities/HomeActivity.java
+++ app/src/main/java/com/ispc/gestorstock/activities/HomeActivity.java
@@ -2,17 +2,32 @@ package com.ispc.gestorstock.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.ispc.gestorstock.R;
 import com.ispc.gestorstock.dialogs.SignOutDialog;
+import com.ispc.gestorstock.models.Product;
 import com.ispc.gestorstock.providers.AuthProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
     SignOutDialog mDialog;
     AuthProvider mAuth;
+    List<Product> products = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +36,13 @@ public class HomeActivity extends AppCompatActivity {
 
         mDialog = new SignOutDialog();
         mAuth = new AuthProvider();
+
+        for (int i = 0; i < 300; i++){
+            products.add(new Product("1", "Test " + i, 10, 10));
+        }
+        Log.d("HOME", products.toString());
+        loadProducts();
+
     }
 
     @Override
@@ -36,4 +58,40 @@ public class HomeActivity extends AppCompatActivity {
     public void doNegativeClick(){
         mDialog.dismiss();
     }
+
+    public void loadProducts(){
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.activity_home, null);
+
+        TableLayout table = (TableLayout) v.findViewById(R.id.table_products);
+
+        TableRow tbRow = new TableRow(this);
+        tbRow.setOrientation(LinearLayout.VERTICAL);
+
+        for (Product p :
+                products) {
+            TableRow tr = new TableRow(this);
+            TextView tvName = new TextView(this);
+            tvName.setText(p.getName());
+            tr.addView(tvName);
+
+            TextView tvStock = new TextView(this);
+            tvStock.setText(String.valueOf(p.getStock()));
+            tvStock.setGravity(Gravity.CENTER);
+            tr.addView(tvStock);
+
+            TextView tvPrice = new TextView(this);
+            tvPrice.setText("$ " + String.valueOf(p.getPrice()));
+            tvPrice.setGravity(Gravity.END);
+            tr.addView(tvPrice);
+
+            table.addView(tr);
+        }
+
+        // Display the view
+        setContentView(v);
+
+    }
+
+
 }
---
+++ app/src/main/java/com/ispc/gestorstock/models/Product.java
@@ -0,0 +1,43 @@
+package com.ispc.gestorstock.models;
+
+public class Product {
+    String id;
+    String name;
+    float price;
+    float stock;
+
+    public Product(String id, String name, float price, float stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getStock() {
+        return stock;
+    }
+
+    public void setStock(float stock) {
+        this.stock = stock;
+    }
+}
